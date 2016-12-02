0e0f6a70d137414a206cb4f500b4864ffb5bf3e7
Felix Hambrecht <fhambrec@mail.uni-mannheim.de>
2016-12-02 22:43:09 +0100

    ...

--- src/main/java/com/backend/fetch/ScheduledStationFetch.java
+++ src/main/java/com/backend/fetch/ScheduledStationFetch.java
@@ -4,6 +4,10 @@ import java.text.SimpleDateFormat;
 
 import com.backend.entities.Messstelle;
 import com.backend.entities.MessstelleRepository;
+import de.wsv.pegelonline.webservices.version2_3.api.PegelonlineMessstelle;
+import de.wsv.pegelonline.webservices.version2_3.api.PegelonlinePegelinformation;
+import de.wsv.pegelonline.webservices.version2_3.api.PegelonlineWebservicePortType;
+import de.wsv.pegelonline.webservices.version2_3.api.PegelonlineWebservice_Impl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,29 +26,40 @@ public class ScheduledStationFetch {
     @Autowired
     private MessstelleRepository repository;
 
-    @Scheduled(fixedRate = 1000)
+    @Scheduled(fixedRate = 1000*60)
     public void getMessStellen() {
-        /**
+
         try {
             PegelonlineWebservicePortType port =
                     new PegelonlineWebservice_Impl().getPegelonlineWebservicePort();
 
             PegelonlineMessstelle[] messstellen  = port.getMessstellenList(null, null);
 
+            PegelonlinePegelinformation[] pi =
+                    port.getPegelinformationen(null, null, new String[] {"TRUNSTADT"});
+
+            //Ausgabe der aktuellen Messung der Messstelle TRUNSTADT für alle Parameter
+            for (int i = 0; i < pi.length; i++) {
+                //System.out.println(pi[i].getPegelonlineKoordinate().getHochwert().toString());
+                //System.out.println(pi[i].getPegelonlineKoordinate().getRechtswert().toString());
+                //System.out.println(pi[i].getPegelonlineMessstelle().getName());
+            }
+
             for (PegelonlineMessstelle messstelle: messstellen){
-                log.info(messstelle.getName());
+              //  log.info(messstelle.getName());
+                System.out.println(messstelle.getName());
             }
 
 
         }catch (Exception e){
 
         }
-        */
+
 
         repository.save(new Messstelle("a", "b"));
 
         for(Messstelle mess : repository.findAll()){
-            log.info(mess.toString());
+          //  log.info(mess.toString());
         }
 
 
